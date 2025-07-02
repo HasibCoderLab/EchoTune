e362b44f678eaba8a044c6bb8d41fb1dd98dc2be
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-07-02 18:53:42 +0600

    Update-JavaScript

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-let progress =document.getElementById('progress');
-        let song =document.getElementById('song');
-        let ctrlIcoon =document.getElementById('ctrlIcon');
+const song = document.getElementById('song');
+const progress = document.getElementById('progress');
+const ctrlIcon = document.getElementById('ctrlIcon');
 
-        song.onloadedmetadata = function (){
-            progress.max = song.duration;
-            progress.value = song.current;
+// Set time after audio is loaded
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
 
-        }
+// Play/Pause toggle
+function playPause() {
+  if (song.paused) {
+    song.play();
+    ctrlIcon.classList.remove('bx-play');
+    ctrlIcon.classList.add('bx-pause');
+  } else {
+    song.pause();
+    ctrlIcon.classList.remove('bx-pause');
+    ctrlIcon.classList.add('bx-play');
+  }
+}
 
-
-
-        function playPause(){
-            if(ctrlIcon.classList.contains('bx bx-pause')){
-                song.pause();
-                    ctrlIcon.classList.remove('bx bx-pause');
-                    ctrlIcon.classList.add('bx bx-play');
-                    
-
-            }
-
-            else{
-                song.play();
-                ctrlIcon.classList.add('bx bx-pause');
-                ctrlIcon.classList.remove('bx bx-play')
-
-            }
-        }
-
-
-        if(song.play()){
+// Progress bar update
 setInterval(() => {
-    progress.value = song.currentTime;
-    
+  progress.value = song.currentTime;
 }, 500);
-        }
-        progress.onchange = function(){
-            song.play();
-            song.currentTime = progress.value;
-            ctrlIcon.classList.add('bx bx-pause');
-                ctrlIcon.classList.remove('bx bx-play')
 
-        };
+// Set time when scrubbing
+progress.onchange = function () {
+  song.currentTime = progress.value;
+  song.play();
+  ctrlIcon.classList.remove('bx-play');
+  ctrlIcon.classList.add('bx-pause');
+};
